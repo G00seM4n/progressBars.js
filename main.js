f1c910585e5328b42c727d1c0ad9c2d22dd905b0
g00s3_m4n <petrovih384@gmail.com>
2022-06-24 00:56:17 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    let i = 0;
+
+    function b(i){
+        let e = document.getElementsByClassName('greenBar');
+        let ew = document.getElementById('greyProgress').clientWidth;
+        let w = 0;
+
+        let t = setInterval(function() {
+            if (w >= ew){
+                i++;
+                b(i);
+                clearInterval(t);
+            } else {
+                w++;
+                e[i].style.width = w + 'px';
+                e[i].innerHTML = Math.round((w/ew)*100) + '%';
+            }
+        }, 1);
+
+        if(i >= e.length) {
+            clearInterval(t);
+        }
+    }
+    b(i);
+}
